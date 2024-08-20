5564f676bc997aad0492ef7b309bac707dc36309
kiss_ivan@hotmail.com <79423340+Ivansallas@users.noreply.github.com>
2024-08-19 21:25:13 -0300

    2

---
+++ DeclaraçãodeVariaveis.js
@@ -0,0 +1,31 @@
+// Uso de as simples ou duplas
+const texto1 = "Olá, mundo!";
+const texto2 = 'Olá, mundo!';
+
+// Concatenado variaveis
+const citacao = "Meu nome é ";
+const meuNome = "imprime";
+
+// imprimindo vareaveis
+console.log(texto1)
+console.log(texto2)
+
+
+// concatenação (+)
+console.log(citacao + meuNome)
+
+// boolean
+// true -> verdadeiro
+// false -> falso
+
+const primeiroNumero = 5;
+const segundoNumero = 5;
+let cadastroAtivado = true;
+
+console.log(primeiroNumero === segundoNumero);
+
+const nome1 = "João";
+const nome2 = "Maria";
+
+console.log(nome1 === nome2)
+
