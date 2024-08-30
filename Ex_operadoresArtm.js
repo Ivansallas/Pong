2f196fb9508de0cc69d1f78a314615f90a41f335
Ivansallas <Kiss_ivan@hotmail.com>
2024-08-30 09:22:34 -0300

    Create Ex_operadoresArtm.js

---
+++ Ex_operadoresArtm.js
@@ -0,0 +1,24 @@
+//operadores aritimeticos
+let a = 10;
+let b = 5;
+let soma = a + b;
+let subitração = a - b;
+let multiplição = a * b;
+let divissão = a / b;
+
+console.log(soma); // 15
+console.log(subitração); // 5
+console.log(multiplição); // 50
+console.log(divissão); // 2
+
+
+//i = a
+for (let i = 0; i < 5; i++) {
+    console.log(i);
+}
+let frutas = ["maçã", "banana", "laranja"];
+
+for (let i = 0; i < frutas.length; i++) {
+    console.log(frutas[i]);
+}
+
